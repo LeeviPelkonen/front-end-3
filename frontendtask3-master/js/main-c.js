1a57d10fabefe99969e2ed16cd7267de17473436
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2018-11-14 17:02:30 +0200

    front 3

---
+++ frontendtask3-master/js/main-c.js
@@ -0,0 +1,42 @@
+// Create function 'showImages' which
+// loads images.json which has links to images as an array.
+
+// create a loop which builds the following HTML from every image in the array: 
+/*
+<li>
+    <figure>
+        <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
+        <figcaption>
+            <h3>Title</h3>
+        </figcaption>
+    </figure>
+</li>
+*/
+// Make the above HTML by using DOM methods.
+// Create elements with createElement()
+// Add attributes with setAttribute()
+// Add elements with appendChild
+// When the above HTML is ready append it to the <ul> element
+
+
+
+const showImages = (images) => {
+
+  const html = images.forEach(image => {
+
+        let aEl = document.createElement("a");
+        aEl.setAttribute('src', 'img/original/${image.mediaUrl}');
+      }
+  );
+  let replace = document.getElementsByTagName('ul');
+  replace.appendChild(html);
+
+};
+
+let json = new Request('./images.json');
+console.log(json);
+fetch(json)
+.then(function(response) { return response.json(); })
+.then(function(data) {
+  showImages(data);
+});
